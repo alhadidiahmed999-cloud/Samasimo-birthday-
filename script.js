a7478c767d4d59b7c1eacc826f6a505491f1e050
alhadidiahmed999-cloud <alhadidiahmed999@gmail.com>
2025-12-09 04:40:15 +0200

    Add click event to ribbon for envelope opening

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-function openLetter(){document.getElementById('letter').classList.remove('hidden');}
+document.getElementById("ribbon").onclick = function(){
+    this.classList.add("hidden");
+    document.getElementById("envelope").classList.add("open");
+};
